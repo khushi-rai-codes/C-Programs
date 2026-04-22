82b46e98f28e5e4b42142bb4e979cd7dd43ffe2a
khushi-rai-codes <kr14052006@gmail.com>
2026-04-22 20:28:08 +0530

    Create smallest_number_in_array.c

---
+++ smallest_number_in_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int arr[5], smallest, i;
+    printf("Enter 5 numbers:\n");
+    for(i = 0; i < 5; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    smallest = arr[0];
+    for(i = 1; i < 5; i++)
+    {
+        if(arr[i] < smallest)
+        {
+            smallest = arr[i];
+        }
+    }
+    printf("Smallest number = %d", smallest);
+
+    return 0;
+}
