c9b584f3f7f46e16e8c16ec055e3ce7c414208e8
khushi-rai-codes <kr14052006@gmail.com>
2026-04-29 19:22:55 +0530

    Create transpose_matrix.c

---
+++ transpose_matrix.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int rows, cols;
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+    int matrix[rows][cols];
+    printf("Enter matrix elements:\n");
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < cols; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    printf("Transpose matrix:\n");
+    for(int i = 0; i < cols; i++)
+    {
+        for(int j = 0; j < rows; j++)
+        {
+            printf("%d ", matrix[j][i]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
