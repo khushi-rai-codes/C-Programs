486314d6e94cc800363294130c7f7c6ead1120e9
khushi-rai-codes <kr14052006@gmail.com>
2026-05-05 20:13:14 +0530

    Create remove_spaces_from_string.c

---
+++ remove_spaces_from_string.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[200], result[200];
+    int j = 0;
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    for(int i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] != ' ' && str[i] != '\n')
+        {
+            result[j++] = str[i];
+        }
+    }
+    result[j] = '\0';
+    printf("String without spaces: %s", result);
+    return 0;
+}
