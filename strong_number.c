fbbc29a5919d9fb3669b8ac6c18dccd751960a43
khushi-rai-codes <kr14052006@gmail.com>
2026-05-16 19:11:06 +0530

    Create strong_number.c

---
+++ strong_number.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int factorial(int n)
+{
+    int fact = 1;
+    for(int i = 1; i <= n; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
+int main()
+{
+    int num, original, remainder, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    original = num;
+    while(num > 0)
+    {
+        remainder = num % 10;
+        sum += factorial(remainder);
+        num = num / 10;
+    }
+    if(sum == original)
+        printf("%d is a Strong Number", original);
+    else
+        printf("%d is not a Strong Number", original);
+    return 0;
+}
