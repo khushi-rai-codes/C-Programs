e3e9ccbfe72c6b102c1b07a4e4bb327a5b1f55b8
khushi-rai-codes <kr14052006@gmail.com>
2026-06-21 22:02:01 +0530

    Create palindromic_number_pyramid.c

---
+++ palindromic_number_pyramid.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n = 5;
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = i; j < n; j++)
+            printf(" ");
+        for(int j = i; j >= 1; j--)
+            printf("%d", j);
+        for(int j = 2; j <= i; j++)
+            printf("%d", j);
+        printf("\n");
+    }
+    return 0;
+}
